e0f97fc789a1541ed1117a3b1352f2a836081610
aniketinfeon <aniket.sadaphale@infeon.in>
2022-10-17 11:16:38 +0530

    add new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { SidebarComponent } from './layout/component/sidebar/sidebar.component';
+import { MainComponent } from './layout/component/main/main.component';
+import { FooterComponent } from './layout/component/footer/footer.component';
+import { HeaderComponent } from './layout/component/header/header.component';
+import { LoginModule } from './login/login.module';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/component/dashboard/dashboard.component';
+import { LayoutComponent } from './layout/component/layout/layout.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LayoutComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LoginModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { ForgetPasswordComponent } from './component/forget-password/forget-password.component';
+import { SignInComponent } from './component/sign-in/sign-in.component';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
+
+
+@NgModule({
+  declarations: [
+    ForgetPasswordComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
